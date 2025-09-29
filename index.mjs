008ee449018fac17ef865be64f599b92736cdb2d
prem-ramamoorthy <pkrprem2005@gmail.com>
2025-09-29 10:17:50 +0530

    Backend setup at index.mjs

---
+++ index.mjs
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('API up'));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
